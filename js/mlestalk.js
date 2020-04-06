5e840e5de286d532576793ed3a8c4b2ea7f3153c
jq-rs <jq-rs@mles.io>
2020-04-06 11:54:38 +0300

    Revert "Include to have date of all messages, including history."
    This reverts commit 7ebb7bfe464ea169405a8e79279935370cd845fd.

--- js/mlestalk.js
+++ js/mlestalk.js
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Copyright (c) 2019-2020 MlesTalk developers
+ * Copyright (c) 2019 MlesTalk developers
  */
 var myname = '';
 var mychannel = '';
@@ -27,15 +27,13 @@ var initOk = false;
 const RETIMEOUT = 1500; /* ms */
 const MAXTIMEOUT = 1000*60*5; /* ms */
 const MAXQLEN = 32;
-const RESYNC_TIMEOUT = 5000; /* ms */
+const RESYNC_TIMEOUT = 15000; /* ms */
 const LED_ON_TIME = 500; /* ms */
 const LED_OFF_TIME = 2500; /* ms */
 const SCROLL_TIME = 500; /* ms */
 const ASYNC_SLEEP = 3 /* ms */
 var reconn_timeout = RETIMEOUT;
 var reconn_attempts = 0;
-
-const DATELEN = 13;
 
 var isTokenChannel = false;
 var sipkey;
@@ -46,7 +44,6 @@ var lastWrittenMsg = "";
 var lastMessageSeenTs = 0;
 var lastReconnectTs = 0;
 var lastMessage = {};
-var lastMessageSendOrRcvdDate = "";
 
 var weekday = new Array(7);
 weekday[0] = "Sun";
@@ -167,11 +164,21 @@ function stamptime(msgdate) {
 	if(dd<10) dd='0'+dd;
 	if(mm<10) mm='0'+mm;
 	if(m<10) m='0'+m;
-	return day + ' ' + dd + '.' + mm + '.' + yyyy + ' ' + h + ':' + m;
+	return dd + '.' + mm + '.' + yyyy + ' ' + day + ' ' + h + ':' + m;
 }
 
-function timenow() {
-	return stamptime(new Date());
+function timenow(){
+	var now=new Date(), 
+		dd=now.getDate(),
+		mm=now.getMonth()+1,
+		yyyy=now.getFullYear(),
+		h=now.getHours(), 
+		m=now.getMinutes(), 
+		day=weekday[now.getDay()];
+	if(dd<10) dd='0'+dd;
+	if(mm<10) mm='0'+mm;
+	if(m<10) m='0'+m;
+	return dd + '.' + mm + '.' + yyyy + ' ' + day + ' ' + h + ':' + m;
 }
 
 var can_notify = false;
@@ -514,10 +521,6 @@ webWorker.onmessage = function(e) {
 			}
 
 			if(idtimestamp[uid] <= msgTimestamp) {
-				var li;
-				var date;
-				var time;
-
 				if(isFull && 0 == message.length) /* Ignore init messages in timestamp processing */
 					break;
 				
@@ -539,23 +542,19 @@ webWorker.onmessage = function(e) {
 				if(0 == message.length)
 					break;
 
-				date = update_dateval(dateString);
-				if(date) {
-					/* Update new date header */
-					li = '<li class="new"> - <span class="name">' + date + '</span> - </li>';
-					$('#messages').append(li);
-				}
-				time = update_time(dateString);
+				var li;
+
+				dateString = update_datestring(dateString);
 
 				/* Check first is it a text or image */
 				if(isImage) {
 					if (uid != myname) {
-						li = '<div id="' + duid + '' + idhash[duid] + '"><li class="new"><span class="name">' + uid + '</span> ' + time +
-							 '<img class="image" src="' + message + '" height="100px" data-action="zoom" alt="">';
+						li = '<div id="' + duid + '' + idhash[duid] + '"><li class="new"><span class="name">' + uid + '</span> ' + dateString 
+							+ '<img class="image" src="' + message + '" height="100px" data-action="zoom" alt="">';
 
 						}
 					else {
-						li = '<div id="' + duid + '' + idhash[duid] + '"><li class="own"> ' + time
+						li = '<div id="' + duid + '' + idhash[duid] + '"><li class="own"> ' + dateString
 							+ '<img class="image" src="' + message + '" height="100px" data-action="zoom" alt="">';
 
 					}
@@ -564,10 +563,10 @@ webWorker.onmessage = function(e) {
 				else {
 					if (uid != myname) {
 						li = '<div id="' + duid + '' + idhash[duid] + '"><li class="new"><span class="name"> ' + uid + '</span> '
-							+ time + "" + autolinker.link( message ) + '</li></div>';
+							+ dateString + "" + autolinker.link( message ) + '</li></div>';
 					}
 					else {
-						li = '<div id="' + duid + '' + idhash[duid] + '"><li class="own"> ' + time + "" + autolinker.link( message ) + '</li></div>';
+						li = '<div id="' + duid + '' + idhash[duid] + '"><li class="own"> ' + dateString + "" + autolinker.link( message ) + '</li></div>';
 					}
 				}
 
@@ -627,25 +626,16 @@ webWorker.onmessage = function(e) {
 	}
 }
 
-function update_dateval(dateString) {
-	var lastDate = lastMessageSendOrRcvdDate;
-	const begin = weekday[0].length+2;
-	const end = DATELEN+1;
-	if (lastDate != "" &&
-		dateString.slice(begin, end) == lastDate.slice(begin, end))
-	{
-		return null;
+function update_datestring(dateString) {
+	var now = timenow();
+	if (dateString.charAt(1) == now.charAt(0) && dateString.charAt(2) == now.charAt(1) &&
+		dateString.charAt(4) == now.charAt(3) && dateString.charAt(5) == now.charAt(4) &&
+		dateString.charAt(7) == now.charAt(6) && dateString.charAt(8) == now.charAt(7) &&
+		dateString.charAt(9) == now.charAt(8) && dateString.charAt(10) == now.charAt(9)) {
+		dateString = dateString.slice(13 + weekday[0].length, dateString.length);
+		dateString = "[" + dateString;
 	}
-	else {
-		var dateval = dateString.slice(1, DATELEN + weekday[0].length - 1);
-		lastMessageSendOrRcvdDate = dateString;
-		return dateval;
-	}
-}
-
-function update_time(dateString) {
-	var time = "[" + dateString.slice(DATELEN + weekday[0].length, dateString.length);
-	return time;
+	return dateString;
 }
 
 function do_notify(uid, channel, msgTimestamp, message) {
@@ -730,21 +720,14 @@ function send_data(cmd, uid, channel, data, isFull, isImage, isMultipart, isFirs
 function update_after_send(message, isFull, isImage) {
 
 	var dateString = "[" + timenow() + "] ";
-	var date = update_dateval(dateString);
-	var time = update_time(dateString);
+	dateString = update_datestring(dateString);
 	var li;
 
-	if(date) {
-		/* Update new date header */
-		li = '<li class="own"> - <span class="name">' + date + '</span> - </li>';
-		$('#messages').append(li);
-	}
-
 	if(!isImage) {
-		li = '<div id="owner' + ownid + '"><li class="own"> ' + time + "" + autolinker.link( message ) + '</li></div>';
+		li = '<div id="owner' + ownid + '"><li class="own"> ' + dateString + "" + autolinker.link( message ) + '</li></div>';
 	}
 	else {
-		li = '<div id="owner' + ownid + '"><li class="own"> ' + time
+		li = '<div id="owner' + ownid + '"><li class="own"> ' + dateString
 			 + '<img class="image" src="' + message + '" height="100px" data-action="zoom" alt=""></li></div>';
 	}
 
