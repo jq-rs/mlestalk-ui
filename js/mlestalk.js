36095d5d5b7401016c16f64189fccf93cae57ecb
jq-rs <jq-rs@mles.io>
2020-04-06 14:10:53 +0300

    Update to webworker submodule.

--- js/mlestalk.js
+++ js/mlestalk.js
@@ -179,7 +179,7 @@ var will_notify = false;
 var isCordova = false;
 var isReconnect = false;
 
-var webWorker = new Worker('mlestalk-webworker/js/webworker.js');
+var webWorker = new Worker('webworker/js/webworker.js');
 
 function onPause() {
 	will_notify = true;
