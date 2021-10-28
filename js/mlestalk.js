40f6dfbe27e6366025bb180409a011213fde1da9
jq-rs <jq-rs@mles.io>
2021-10-28 09:11:29 +0300

    Fix message hashing handling with multichannel.

--- js/mlestalk.js
+++ js/mlestalk.js
@@ -50,7 +50,7 @@ const LISTING_SHOW_TIMER = 3500; /* ms */
 const RETIMEOUT = 1500; /* ms */
 const MAXTIMEOUT = 1000 * 60 * 4; /* ms */
 const MAXQLEN = 1000;
-const RESYNC_TIMEOUT = 4000; /* ms */
+const RESYNC_TIMEOUT = 2500; /* ms */
 const LED_ON_TIME = 500; /* ms */
 const LED_OFF_TIME = 2500; /* ms */
 const SCROLL_TIME = 500; /* ms */
@@ -801,7 +801,6 @@ function processForwardSecrecyOff(uid, channel) {
 function msgHashHandle(uid, channel, msgTimestamp, mhash) {
 	if(!gIdTimestamp[channel])
 		gIdTimestamp[channel] = {};
-
 	let timedict = gIdTimestamp[channel][uid];
 	if (!timedict) {
 		gIdTimestamp[channel][uid] = {};
