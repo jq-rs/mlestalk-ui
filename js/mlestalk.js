ebbaba8ba7cbe7d22ab4197fb56b18798c9c9cea
jq-rs <jq-rs@mles.io>
2024-05-03 08:42:14 +0300

    Bump version.

--- js/mlestalk.js
+++ js/mlestalk.js
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2019-2024 MlesTalk developers
  */
-const VERSION = "3.0.12beta";
+const VERSION = "3.0.13beta";
 const UPGINFO_URL = "https://mles.io/mlestalk/mlestalk_version.json";
 
 let gMyName = {};
